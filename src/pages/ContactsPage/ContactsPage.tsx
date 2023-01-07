8f6ca296d0acd119d040172730195537e05de61e
Tatiana Rusak <tatianarusak85@gmail.com>
2023-01-07 22:53:13 +0300

    refactor: arrange styles for contacts-page

--- src/pages/ContactsPage/ContactsPage.tsx
+++ src/pages/ContactsPage/ContactsPage.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { ToastContainer } from 'react-toast';
 import ContactForm from '../../components/ContactForm/ContactForm';
 import Contacts from '../../components/Contacts/Contacts';
+import './ContactsPage.scss';
 
 const ContactsPage = () => {
   return (
     <div className="contacts">
-      <h2 className="contacts__subheader">if you have any suggestions</h2>
-      <h1 className="contacts__header">send me an email</h1>
+      <h2 className="contacts__subheader subheader">if you have any suggestions</h2>
+      <h1 className="contacts__header header">send me an email</h1>
       <ContactForm />
       <Contacts />
       <ToastContainer delay={5000} position="top-center" />
